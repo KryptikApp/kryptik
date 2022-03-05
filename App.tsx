6e56b6f41fd06458c8bc9f3d429706035b993999
jettblu <jettblu.fabrications@gmail.com>
2022-03-04 22:35:50 -0500

    Update screen folder path

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, Image, View, Button } from 'react-native';
-import LandingScreen from './app/assets/screens/LandingScreen';
+import LandingScreen from './app/screens/LandingScreen';
 
 
 export default function App() {
