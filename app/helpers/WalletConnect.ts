ef265194d4cb192e5276f49ad46d04e511e8c3d1
jettblu <jettblu.fabrications@gmail.com>
2022-03-22 19:22:39 -0400

    Add basic wallet connect file

---
+++ app/helpers/WalletConnect.ts
@@ -0,0 +1,71 @@
+import WalletConnect from "@walletconnect/client";
+
+// Create connector
+const connector = new WalletConnect(
+  {
+    // Required
+    uri: "wc:8a5e5bdc-a0e4-47...TJRNmhWJmoxdFo6UDk2WlhaOyQ5N0U=",
+    // Required
+    clientMeta: {
+      description: "WalletConnect Developer App",
+      url: "https://walletconnect.org",
+      icons: ["https://walletconnect.org/walletconnect-logo.png"],
+      name: "WalletConnect",
+    },
+  },
+);
+
+// Subscribe to session requests
+connector.on("session_request", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Handle Session Request
+
+  /* payload:
+  {
+    id: 1,
+    jsonrpc: '2.0'.
+    method: 'session_request',
+    params: [{
+      peerId: '15d8b6a3-15bd-493e-9358-111e3a4e6ee4',
+      peerMeta: {
+        name: "WalletConnect Example",
+        description: "Try out WalletConnect v1.0",
+        icons: ["https://example.walletconnect.org/favicon.ico"],
+        url: "https://example.walletconnect.org"
+      }
+    }]
+  }
+  */
+});
+
+// Subscribe to call requests
+connector.on("call_request", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Handle Call Request
+
+  /* payload:
+  {
+    id: 1,
+    jsonrpc: '2.0'.
+    method: 'eth_sign',
+    params: [
+      "0xbc28ea04101f03ea7a94c1379bc3ab32e65e62d3",
+      "My email is john@doe.com - 1537836206101"
+    ]
+  }
+  */
+});
+
+connector.on("disconnect", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Delete connector
+});
